bf52b1779807677e8dd794e7fc09d0c266984af9
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-18 15:20:16 -0200

    Push 1

--- footline/settings.py
+++ footline/settings.py
@@ -134,6 +134,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # In settings.py
 CHANNEL_LAYERS = {
     "default": {
