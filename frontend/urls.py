bcb597a03ee33c9122dbb10716902185c9f049e9
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-18 13:45:28 -0200

    Fixes: Heroku push

--- frontend/urls.py
+++ frontend/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from .views import index
+from .views import index, admin
 
 urlpatterns = [
+    url(r'^manager/', admin),
     url(r'^', index),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'register.html')
+
+def admin(request):
+    return render(request, 'admin.html')
