b1353a9e01b67afc1af4429f70559b5166f2cc2b
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-18 15:22:52 -0200

    Push 1

--- footline/settings.py
+++ footline/settings.py
@@ -134,7 +134,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static'),
+STATIC_ROOT   = os.path.join(PROJECT_ROOT,'static').replace('\\', '/'),
 
 
 # In settings.py
