1b3dcb3fa63d55c6c6f24f4f14f92232cef6c86b
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-20 10:02:24 -0300

    ok

--- footline/asgi.py
+++ footline/asgi.py
@@ -1,9 +1,5 @@
 import os
 import channels.asgi
-from whitenoise.django import DjangoWhiteNoise
-from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "footline.settings")
 channel_layer = channels.asgi.get_channel_layer()
-if settings.HEROKU:
-    application = DjangoWhiteNoise(channel_layer)
