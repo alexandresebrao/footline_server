1f8cfd19319366e5ce91fadf697a5cb8bb1fc31f
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-20 10:01:19 -0300

    ok

--- footline/settings.py
+++ footline/settings.py
@@ -135,15 +135,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-if HEROKU:
-    STATIC_ROOT = 'static'
-else:
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 if HEROKU:
     CHANNEL_LAYERS = {
