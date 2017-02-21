9654343069a085004bc2844acca0064627aab0e1
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-21 13:59:01 -0300

    Push first register

--- footline/settings.py
+++ footline/settings.py
@@ -36,7 +36,8 @@ except:
 if HEROKU:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = [
