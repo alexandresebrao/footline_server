f2d1fb9d36045966e25894762c918435434d86c6
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-22 14:26:23 -0300

    ok

--- footline/settings.py
+++ footline/settings.py
@@ -140,7 +140,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
