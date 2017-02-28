c2f039e49c74c60a6b53b25f34617a072144b7bf
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-02-28 14:42:34 -0300

    ok

--- api/v1/serializers/user.py
+++ api/v1/serializers/user.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(allow_blank=False)
 
     def create(self, validated_data):
 
         user = User.objects.create(
-            username=validated_data['username']
+            username=validated_data['username'], email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
